e8c6f73074af86464c0d420a22b0fc394f4c196f
Jubril <jubrilbucknor@gmail.com>
2023-03-06 08:17:17 +0100

    First look of random choice picker

---
+++ main.js
@@ -0,0 +1,70 @@
+const tagsEl = document.getElementById("tags")
+const textArea = document.getElementById("floatingTextarea")
+
+textArea.focus()
+
+textArea.addEventListener("keyup", (e) => {
+    createTags(e.target.value )
+
+    if(e.key === "Enter") {
+        setTimeout(() => {
+            e.target.value = ""
+        }, 10)
+
+        randomSelect()
+    }
+})
+
+const createTags = (input) => {
+    const tags = input.split(",").filter(tag => tag.trim() !== " ").map(tag => tag.trim())
+
+    tagsEl.innerHTML = ""
+    
+    tags.forEach(tag => {
+        const tagEl = document.createElement("span")
+        tagEl.classList.add("tag")
+        tagEl.innerText = tag
+        tagsEl.appendChild(tagEl)
+
+    });
+}
+
+function randomSelect() {
+    const times = 30
+
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag()
+
+        highlight(randomTag)
+
+        setTimeout(() => {
+            unhighlight(randomTag)
+        }, 100);
+    }, 100);
+
+
+    setTimeout(() => {
+        clearInterval(interval)
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag()
+
+            highlight(randomTag)
+        }, 100)
+    }, times * 100)
+}
+
+function pickRandomTag() {
+    const tags = document.querySelectorAll(".tag")
+    return tags[Math.floor(Math.random() * tags.length)] 
+         
+}
+
+function highlight(tag) {
+    tag.classList.add("highlight")
+}
+
+function unhighlight(tag) {
+    tag.classList.remove("highlight")
+}
+
